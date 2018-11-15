aa720dcac1ae37fa1db7f03df8052cb202ed3deb
jayrokevin <38084216+jayrokevin@users.noreply.github.com>
2018-11-15 18:42:05 -0400

    Registros Pendiente Diseño

--- Kevin Cardenas/admin_section/farmacia.php
+++ Kevin Cardenas/admin_section/farmacia.php
@@ -1,9 +1,26 @@
 <?php
+
 if (isset($_POST['enviarfarmacia'])) 
 {
+
+//recibimos el nombre 
+$nombre = $_POST['nombre'];
+
+//recibimos la direccion 
+$direccion = $_POST['direccion'];
+
+//recibimos la descripcion
+$descripcion = $_POST['descripcion'];
+
+//recibimos horario desde ; hasta 
 $tipod = $_POST['desde'];
 $tipoh = $_POST['hasta'];
 
+//consulta para tabla lugar * falta logo, mapa, etc
+echo "insert into lugar (nombre_lugar,direccion,descripcion) values ($nombre,$direccion,$descripcion)";
+
+
+//recibimos valores 1 o 0 de los checkbox
 if (isset($_POST['turno']) && $_POST['parqueo'] = '1')      
 $turno = 1;
 else
@@ -27,6 +44,7 @@ $domicilio = 0;
 
 $horario = "$tipod a $tipoh";
 
+//consulta para tabla farmacia
 echo "insert into Farmacia (horario,turno,vacunas,servicio_enfermeria,entrega_domicilio) values ($horario,$turno,$vacunas,$enfermeria,$domicilio)";
 
 
