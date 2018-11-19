8f267e47b4b047c29a421425178d9c33e863b7bc
Pablof3 <Pablohy2898@hotmail.com>
2018-11-19 11:59:03 -0400

    busqueda de sitios backend, vista

--- busqueda/controller/busqueda.php
+++ busqueda/controller/busqueda.php
@@ -9,74 +9,28 @@ $mRestaurante=new mRestaurante;
 $mFarmacia= new mFarmacia;
 $mGeneral=new mGeneral;
 $data;
-$buscar=(isset($_GET['buscar']))?$_GET['buscar']:'';
-//lat long
+
+$buscar=(isset($_GET['buscar']))?$_GET['buscar']:'';//lat long
 $lugar=(isset($_GET['lugar']))?$_GET['lugar']:'';
-$categoria=(isset($_GET['categoria']))?$_GET['categoria']:'';
+$categoria=(isset($_GET['categoria']))?$_GET['categoria']:[''];
 $distanciaRad=(isset($_GET['distanciaRad']))?$_GET['distanciaRad']:'';
-$orden=(isset($_GET['orden']))?$_GET['orden']:'';
-$puntaje=(isset($_GET['puntaje']))?$_GET['puntaje']:'';
+$orden=(isset($_GET['orden']))?$_GET['orden']:'all';
+$puntaje=(isset($_GET['puntaje']))?$_GET['puntaje']:[''];
 
-//coincidencias
-if($buscar!='')
-{
-  
-}
+//Buscar
+$sitios=$mGeneral->Buscar($buscar,$categoria,$orden,$puntaje);
 
-
-
-//Categorias
-switch ($categoria) {
-    case '':
-    {
-
-    }
-    break;
-    case 'hoteles':
-    {
-
-    }
-    break;
-    case 'restaurantes':
-    {
-
-    }
-    break;
-    case 'farmacias':
-    {
-
-    }
-    break;
-}
-//Orden
-switch ($orden) {
-    case 'all':
-    {
-
-    }
-    break;
-    case 'pop':
-    {
-
-    }
-    break;
-    case 'ult':
-    {
-
-    }
-    break;
-}
 //Numero de Sitios Por Tipo
 $numeroSitios=['farmacias'=>$mFarmacia->numFarmacias(),'restaurantes'=>$mFarmacia->numFarmacias(),'hoteles'=>$mHotel->numHoteles()];
-//NUmero de Stios por Puntaje
-//02
+//Numero de Stios por Puntaje
 $num02=$mGeneral->numeroSitiosPorPuntaje(0)+$mGeneral->numeroSitiosPorPuntaje(1)+$mGeneral->numeroSitiosPorPuntaje(2);
 $num34=$mGeneral->numeroSitiosPorPuntaje(3)+$mGeneral->numeroSitiosPorPuntaje(4);
 $num5=$mGeneral->numeroSitiosPorPuntaje(5);
 $sitiosPuntaje=['pts5'=>$num5, 'pts34'=>$num34, 'pts02'=>$num02];
 
-
-$data=['numSitios'=>$numeroSitios,'sitiosPuntaje'=>$sitiosPuntaje];
+$data=['numSitios'=>$numeroSitios,
+        'sitiosPuntaje'=>$sitiosPuntaje,
+        'sitios'=>$sitios];
 
 
 
