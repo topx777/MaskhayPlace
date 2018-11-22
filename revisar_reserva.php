1445cd0765d249a66e299fe6d6b581660db7d911
Comando777 <44825428+Comando777@users.noreply.github.com>
2018-11-22 01:22:16 -0400

    Update revisar_reserva.php

--- revisar_reserva.php
+++ revisar_reserva.php
@@ -61,7 +61,7 @@ if(isset($_SESSION["filtro"])) {
 <body class="fixed-nav sticky-footer" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-default fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html"><img src="assets/admin/img/logo.png" data-retina="true" alt="" width="165" height="36"></a>
+    <a class="navbar-brand" href="index-2.php"><img src="assets/admin/img/logo.png" data-retina="true" alt="" width="165" height="36"></a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -74,19 +74,19 @@ if(isset($_SESSION["filtro"])) {
           </a>
           <ul class="sidenav-second-level collapse" id="collapseMylistings">
             <li>
-              <a href="listings.html">Pending <span class="badge badge-pill badge-primary">6</span></a>
+              <a href="listings.html">Pending <span class="badge badge-pill badge-primary">-</span></a>
             </li>
 			<li>
-              <a href="listings.html">Active <span class="badge badge-pill badge-success">6</span></a>
+              <a href="listings.html">Active <span class="badge badge-pill badge-success">-</span></a>
             </li>
 			<li>
-              <a href="listings.html">Expired <span class="badge badge-pill badge-danger">6</span></a>
+              <a href="listings.html">Expired <span class="badge badge-pill badge-danger">-</span></a>
             </li>
           </ul>
         </li>
     </div>
   </nav>
-  <!-- /Navigation-->
+<!-- /Navigation-->
   <div class="content-wrapper">
     <div class="container-fluid">
       <!-- Breadcrumbs-->
